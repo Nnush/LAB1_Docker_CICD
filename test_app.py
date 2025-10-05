d750c7e572b99baf3ba12e7171ef8f3d7aebc4a0
anoushka <anoushka.shendurkar@sdbi.in>
2025-10-05 14:44:49 +0530

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,9 @@
+
+from app import app
+
+def test_home_message():
+    client = app.test_client()
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert resp.data.decode() == "Hello, CI/CD World!"
+
