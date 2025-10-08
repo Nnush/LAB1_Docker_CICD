c6d5db3c0109ecba0fac486ed3bd0274978ec995
Nnush <anoushka.shendurkar@sdbi.in>
2025-10-08 21:57:59 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,5 +5,5 @@ def test_home_message():
     client = app.test_client()
     resp = client.get("/")
     assert resp.status_code == 200
-    assert resp.data.decode() == "Hello, CI/CD World!"
+    assert resp.data.decode() == "Hello, CI/CD World4!"
 
